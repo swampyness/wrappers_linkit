c38404d3c186d5245bc9c7c234c3b903232b9cc2
swampyness <swampyness@gmail.com>
2016-04-14 01:17:24 +1000

    Update wrapper_linkit_wifi.h

--- wrapper_linkit_wifi/wrapper_linkit_wifi.h
+++ wrapper_linkit_wifi/wrapper_linkit_wifi.h
@@ -10,11 +10,21 @@ class LinkItWifi {
 		#define WIFI_PASSWORD	"your_wifi_password"
 		#define WIFI_AUTH		LWIFI_WPA			// LWIFI_OPEN LWIFI_WPA LWIFI_WEP			
 		#define DEBUG			1
-		#define CONTENT_HTML		0
-		#define CONTENT_IMAGE		1
-		#define CONTENT_FRAME		2		
+		#define CONTENT_HTML	0
+		#define CONTENT_IMAGE	1
+		#define CONTENT_FRAME	2		
 		LWiFiClient client;
 		String html_body;
+		String html_title;
+		String server_ip;
+		String server_mac;
+		String server_subnet_mask;
+		String server_gateway;
+		String ap_ssid;
+		String ap_signal;
+		String ap_count;
+		String ap_list[20];
+		String client_ip;
 		int data_length;
 		int server_available;
 		int client_connected;
@@ -25,7 +35,10 @@ class LinkItWifi {
 		void status();
 		void html_build();
 		void html_init();
+		String link(String url, String label);
+		String span(String span_name, String span_data, String span_class);
 	private:
+		long timer_start;
 		String html_header;
 		String html_banner;
 		String html_footer;
